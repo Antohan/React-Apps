60cc65787086a1f5afc04208a21370128ea53a5a
asukhodolets <theo19@mail.ru>
2018-01-15 22:02:58 +0700

    add axios and connected Mock API

--- todo-list/README.md
+++ todo-list/README.md
@@ -1,4 +1,4 @@
 
 ## Simple To-do list React app
 
-- Create with create-react-app
+# Create with create-react-app
--- todo-list/src/TodoApp.js
+++ todo-list/src/TodoApp.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 import Title from './Title';
+import axios from 'axios';
 
 class TodoApp extends Component {
     constructor(props) {
@@ -9,29 +10,53 @@ class TodoApp extends Component {
         this.state = {
             data: []
         }
+        this.apiUrl = 'https://5a5cb5aad6221a0012962cf8.mockapi.io/todos';
+    }
+
+    componentDidMount() {
+        axios.get(this.apiUrl)
+            .then((res) => {
+                this.setState({data: res.data});
+            })
+            .catch((res) => {
+                errors: res.statusText
+            })
     }
 
     addTodo(val) {
-        const todo = {text: val, id: window.id++}
+        /*const todo = {text: val, id: window.id++}
         this.state.data.push(todo);
         this.setState({
             data: this.state.data
-        });
+        });*/
+        const todo = {text: val}
+
+        axios.post(this.apiUrl, todo)
+            .then((res) => {
+                this.state.data.push(res.data);
+                this.setState({data: this.state.data});
+            });
     }
 
     handleRemove(id) {
         const remainder = this.state.data.filter((todo) => {
             if(todo.id != id) return todo;
         });
+        /*
         this.setState({
             data: remainder
         });
+        */
+        axios.delete(this.apiUrl + '/' + id)
+            .then((res) => {
+                this.setState({data: remainder});
+            });
     }
 
     render() {
         return (
             <div>
-                <Title />
+                <Title todoCount={this.state.data.length} />
                 <TodoForm addTodo={this.addTodo.bind(this)} />
                 <TodoList
                     todos={this.state.data}
